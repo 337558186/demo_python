e3763e5a3c9831402a369370b975f1faed936fa5
ZhangWenQiang <zwq0552@gmail.com>
2023-08-13 12:40:44 +0800

    update

--- 1-常用三方库/scheduled_tasks.py
+++ 1-常用三方库/scheduled_tasks.py
@@ -4,7 +4,7 @@
 @desc :  定时任务
 """
 import datetime
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler  # pip install apscheduler==3.6.1
 
 def task(name):
     print('%s告诉你现在时间是:%s'.format(name, datetime.datetime.now()))
@@ -12,5 +12,5 @@ def task(name):
 
 # 该任务将会在2022-05-20 13:14:52执行一次
 scheduler = BlockingScheduler()
-scheduler.add_job(task, 'date', run_date=datetime.datetime(2022, 5, 20, 13, 14, 52), args=['autofelix'], id='task')
+scheduler.add_job(task, 'date', run_date=datetime.datetime(2024, 5, 20, 13, 14, 52), args=['autofelix'], id='task')
 scheduler.start()
